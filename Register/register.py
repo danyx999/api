ff8f007727e51f6e5d0fdff4a3bdfb27d6134d74
Daniel Zahorec <danko.zahorec@gmail.com>
2023-05-22 11:09:10 +0200

    tests

--- Register/register.py
+++ Register/register.py
@@ -1,6 +1,29 @@
+REGISTER_AMOUNT = 5
+
+
 class Register:
     Customers: list[str]
     Number: int
+    IsOpen = True
 
     def __init__(self) -> None:
-        pass
+        self.Customers = []
+
+    def Open(self) -> None:
+        self.IsOpen = True
+
+    def Close(self) -> None:
+        self.IsOpen = False
+        self.Customers.clear()
+
+    def AddPerson(self, name) -> None:
+        if self.IsOpen:
+            self.Customers.append(name)
+
+    def Transact(self) -> None:
+        if len(self.Customers) > 0:
+            self.Customers.pop(0)
+
+
+class Shop:
+    Registers: list[Register] = []
---
+++ Register/unit_tests.py
@@ -0,0 +1,83 @@
+import pytest
+import unittest
+from register import Register
+
+
+class RegisterTests(unittest.TestCase):
+    def test_Open_WhenRegisterIsClosed_IsOpenIsTrue(self) -> None:
+        target = Register()
+        target.IsOpen = False
+
+        target.Open()
+
+        self.assertTrue(target.IsOpen)
+
+    def test_Open_WhenRegisterIsOpen_IsOpenIsTrue(self) -> None:
+        target = Register()
+        target.IsOpen = True
+
+        target.Open()
+
+        self.assertTrue(target.IsOpen)
+
+    def test_Close_WhenRegisterIsOpen_IsOpenIsFalse(self) -> None:
+        target = Register()
+        target.IsOpen = True
+
+        target.Close()
+
+        self.assertFalse(target.IsOpen)
+
+    def test_Close_WhenRegisterIsClosed_IsOpenIsFalse(self) -> None:
+        target = Register()
+        target.IsOpen = False
+
+        target.Close()
+
+        self.assertFalse(target.IsOpen)
+
+    def test_AddPerson_WhenRegisterIsClosed_CustomersLengthIs0(self) -> None:
+        target = Register()
+        target.IsOpen = False
+
+        target.AddPerson("Peter")
+
+        self.assertEqual(0, len(target.Customers))
+
+    def test_AddPerson_WhenRegisterIsOpen_CustomersLengthIs1(self) -> None:
+        target = Register()
+        target.IsOpen = True
+
+        target.AddPerson("Peter")
+
+        self.assertEqual(1, len(target.Customers))
+
+    def test_Close_WhenRegisterIsOpenAndHasPeople_CustomersLengthIs0(self) -> None:
+        target = Register()
+        target.IsOpen = True
+        target.Customers.append("Peter")
+
+        target.Close()
+
+        self.assertFalse(target.IsOpen)
+        self.assertEqual(0, len(target.Customers))
+
+    def test_Transact_WhenRegisterIsOpenAndHasPeople_CustomerLengthIs0(self):
+        target = Register()
+        target.IsOpen = True
+        target.Customers.append("Peter")
+
+        target.Transact()
+
+        self.assertEqual(0, len(target.Customers))
+
+    def test_Transact_WhenRegisterIsOpen_CustomerLengthIs0(self):
+        target = Register()
+        target.IsOpen = True
+
+        target.Transact()
+
+        self.assertEqual(0, len(target.Customers))
+
+
+unittest.main()
