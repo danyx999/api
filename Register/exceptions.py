844cb0d02df76bb323f800c6637871df1e6c8cbc
danyx999 <danko.zahorec@gmail.com>
2026-06-23 14:18:26 +0200

    Added more exceptions

--- Register/exceptions.py
+++ Register/exceptions.py
@@ -2,3 +2,18 @@ class AllRegistersClosedException(Exception):
     def __init__(self) -> None:
         super().__init__()
         self.Message = f"All registers are closed"
+
+class AllRegistersOpenException(Exception):
+    def __init__(self) -> None:
+        super().__init__()
+        self.Message = f"All registers are open"
+
+class RegisterAlreadyOpenException(Exception):
+    def __init__(self) -> None:
+        super().__init__()
+        self.Message = f"This Register is already open"
+
+class RegisterAlreadyClosedException(Exception):
+    def __init__(self) -> None:
+        super().__init__()
+        self.Message = f"This register is already closed"
