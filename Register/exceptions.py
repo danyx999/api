21f77bbc97ba84f04125d0b81f2e5d642a925550
danyx999 <danko.zahorec@gmail.com>
2026-06-23 14:54:57 +0200

    Adjusted exception classes for easier use and flexibility

--- Register/exceptions.py
+++ Register/exceptions.py
@@ -1,19 +1,25 @@
 class AllRegistersClosedException(Exception):
     def __init__(self) -> None:
-        super().__init__()
-        self.Message = f"All registers are closed"
+        message = f"All registers are closed"
+        super().__init__(message)
 
 class AllRegistersOpenException(Exception):
     def __init__(self) -> None:
-        super().__init__()
-        self.Message = f"All registers are open"
+        message = f"All registers are open"
+        super().__init__(message)
 
 class RegisterAlreadyOpenException(Exception):
     def __init__(self) -> None:
-        super().__init__()
-        self.Message = f"This Register is already open"
+        message = f"This Register is already open"
+        super().__init__(message)
 
 class RegisterAlreadyClosedException(Exception):
     def __init__(self) -> None:
-        super().__init__()
-        self.Message = f"This register is already closed"
+        message = f"This register is already closed"
+        super().__init__(message)
+
+class CannotCloseLastRegisterWithCustomersException(Exception):
+    def __init__(self, registerNum: int) -> None:
+        self.registerNum = registerNum
+        message = f"Can't close register {registerNum}, because it's the last register open and customers are still waiting in the queue"
+        super().__init__(message)
