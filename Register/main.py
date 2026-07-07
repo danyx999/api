55d9f3a0271e3f6fa1742e5e7066b5f8430577bd
danyx999 <danko.zahorec@gmail.com>
2026-07-07 10:06:50 +0200

    Imported necessary classes into main

--- Register/main.py
+++ Register/main.py
@@ -1,5 +1,26 @@
-from register import Register
+from exceptions import (
+    AllRegistersClosedException, 
+    RegisterAlreadyOpenException, 
+    RegisterAlreadyClosedException, 
+    CannotCloseLastRegisterWithCustomersException, 
+    InvalidRegisterNumberException, 
+    RegisterIsClosedException, 
+    NoCustomersInRegisterException
+) 
+from shop import Shop
 
+CUSTOM_EXCEPTIONS = (
+    AllRegistersClosedException,
+    RegisterAlreadyOpenException,
+    RegisterAlreadyClosedException,
+    CannotCloseLastRegisterWithCustomersException,
+    InvalidRegisterNumberException,
+    RegisterIsClosedException,
+    NoCustomersInRegisterException
+)
 
 def main() -> None:
     pass
+
+if __name__ == "__main__":
+    main()
